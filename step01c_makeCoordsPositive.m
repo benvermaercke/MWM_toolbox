e7e3ab3a49d9380de533a5175cd4895877571c0b
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-05-31 19:40:03 -0400

    Apply offset for each arena type and calc sampling rate

--- step01c_makeCoordsPositive.m
+++ step01c_makeCoordsPositive.m
@@ -3,7 +3,7 @@ clc
 
 header_script_MWM
 
-saveIt=1;
+%saveIt=0;
 
 try
     loadName=fullfile('dataSets',databaseName);
@@ -11,25 +11,32 @@ catch
     loadName=fullfile('dataSets_17parameters',filename);
 end
 
-load(loadName,'AllTracks','nTracks','MWMtype')
+load(loadName,'AllTracks','demographics')
 
-if MWMtype==2
-    M=cat(1,AllTracks.data);
-    re_alignment_values=[-min(M(:,2))+5 -min(M(:,3))+5];
+%%% Check if different arenas were used
+arena_IDs=demographics(:,6);
+arena_ID_vector=unique(arena_IDs);
+nArena=length(arena_ID_vector);
+arenaCoords=struct;
+
+%%% Do correction per arena
+for arena_selector=1:nArena
+    sel=arena_IDs==arena_ID_vector(arena_selector);
+        
+    % Find general properties: range of coords
+    M=cat(1,AllTracks(sel).data);
+    sampling_rate=mean(1./diff(M(1:10,2)));
+    re_alignment_values=[-min(M(:,data_cols(1)))+border_size(1) -min(M(:,data_cols(2)))+border_size(2)];
+    
+    % Only apply to selected tracks
+    track_nr_vector=find(sel);
+    nTracks=length(track_nr_vector);
     for iTrack=1:nTracks
-        M=AllTracks(iTrack).data;
-        M(:,2)=M(:,2)+re_alignment_values(1);
-        M(:,3)=M(:,3)+re_alignment_values(2);
-        AllTracks(iTrack).data=M;        
-    end
-else
-    M=AllTracks.data;
-    SR=mean(1./diff(M(1:10,4)));
-    if SR==25
-        %re_alignment_values=[-min(M(:,5))+5 -min(M(:,6))+5];
-        %M(:,5)=M(:,5)+re_alignment_values(1);
-        %M(:,6)=M(:,6)+re_alignment_values(2);
-    else
+        track_nr=track_nr_vector(iTrack);
+        M=AllTracks(track_nr).data;
+        M(:,data_cols(1))=M(:,data_cols(1))+re_alignment_values(1);
+        M(:,data_cols(2))=M(:,data_cols(2))+re_alignment_values(2);
+        AllTracks(track_nr).data=M;
     end
 end
 
@@ -39,7 +46,6 @@ min(M)
 
 if saveIt==1
     %%
-    %AllTracks.data=M;
-    save(loadName,'AllTracks','re_alignment_values','-append')
+    save(loadName,'AllTracks','re_alignment_values','sampling_rate','-append')
     disp('AllTracks overwritten')
 end
