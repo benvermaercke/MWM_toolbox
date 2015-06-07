d9aae553b036e2cf487c9d88612b0aba3c3b8a5f
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-06-06 20:45:04 -0400

    New folder selection

--- header_script_MWM.m
+++ header_script_MWM.m
@@ -7,7 +7,8 @@ if ispc
 else
     if ismac
         data_root='/Users/benvermaercke/Dropbox (Personal)';
-        data_folder=fullfile(data_root,'heatplots tau58');
+        %data_folder=fullfile(data_root,'heatplots tau58');
+        data_folder=fullfile(data_root,'Disconnection SearchStrategies');
     else % server
         [~, user_name] = system('whoami');user_name=user_name(1:end-1);
         %root_folder=fullfile('/home/',user_name,'/MWM_toolbox/'); % temp location
@@ -23,16 +24,20 @@ if ~exist('data_folder','var')
     cd(path_dir)
 end
 
-%%% Construct databse name
-A=strsplit(data_folder,filesep);
-databaseName=A{end};
-databaseName(databaseName==' ')='_';
-
-saveIt=1;
-MWMtype=3; % 1: old | 2: new | 3: open field
-data_cols=[3 4]; % data_cols=[2 3];
-border_size=[5 5];
-%im_size=[200 200];
-
-fprintf('Working from %s\n',data_folder)
-
+if isdir(data_folder)
+    
+    %%% Construct databse name
+    A=strsplit(data_folder,filesep);
+    databaseName=A{end};
+    databaseName(databaseName==' ')='_';
+    
+    saveIt=1;
+    MWMtype=3; % 1: old | 2: new | 3: open field
+    data_cols=[3 4]; % data_cols=[2 3];
+    border_size=[5 5];
+    %im_size=[200 200];
+    
+    fprintf('Working from %s\n',data_folder)
+else
+    fprintf('Specified folder %s not found...\n',data_folder)
+end
