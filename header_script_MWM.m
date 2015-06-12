66b3976bb88336c46a27e97b50bc844746ee55b1
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-06-12 00:05:28 -0400

    Pulling more variables into header script

--- header_script_MWM.m
+++ header_script_MWM.m
@@ -42,18 +42,22 @@ if isdir(data_folder)
             data_cols=[2 3]; % Leuven
             im_size=[200 200];
             kernel_size=35;
+            correction_method=1;
         case 2
             data_cols=[2 3]; % Leuven
             im_size=[200 200];
             kernel_size=35;
+            correction_method=1;
         case 3
             data_cols=[3 4]; % Brisbane
             im_size=[40 40];
             kernel_size=10;
+            correction_method=3;
         case 4
             data_cols=[3 4]; % Brisbane
             im_size=[450 450];
             kernel_size=40;
+            correction_method=3;
         otherwise
     end
     border_size=[5 5];
